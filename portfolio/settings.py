7100995f6f4ce359331f4274737ab17b2f224b1e
TwoEther <lhi0711@naver.com>
2022-12-23 10:06:45 +0900

    final

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-2v@#tll37afct#+57#(p^3_x8cp&x^5b%@5ti$tat+w_erweef
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
